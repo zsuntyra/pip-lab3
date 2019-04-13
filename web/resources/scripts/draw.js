4070b7e72b44cb4e6be18a0d0ef46a39458a9eb0
Suntyra <artyom99@bk.ru>
2019-04-13 20:37:26 +0300

    Add initial version of front

---
+++ web/resources/scripts/draw.js
@@ -0,0 +1,34 @@
+var pt;
+
+document.addEventListener('DOMContentLoaded', function () {
+    pt = document.getElementById('picture').createSVGPoint();
+});
+
+function getPoint(event) {
+    var x = event.clientX;
+    console.log(x);
+    var y = event.clientY;
+    console.log(y);
+    var cursorpt = pt.matrixTransform(document.getElementById('picture').getScreenCTM().inverse());
+    var r = parseFloat(document.getElementById('form:R_text').innerHTML);
+    document.getElementById("form:hiddenX").value = (x + cursorpt.x - 125) * r / 100;
+    document.getElementById("form:Y").value = -(y + cursorpt.y - 125) * r / 100;
+    document.getElementById("form:areaClickBtn").click();
+}
+
+// $("Y").on("change", function() {
+//     var val = parseFloat(this.value) || 1;
+//     if (val > 5) {
+//        this.value = 5;
+//     } else if (val< -3) {
+//         this.value = -3;
+//     }
+//
+// });
+
+function validateY() {
+    var yField = document.getElementById("form.Y");
+    if (parseFloat(yField.value) > 5 || parseFloat(yField.value) < -3) {
+        yField.value = 0.0;
+    }
+}
